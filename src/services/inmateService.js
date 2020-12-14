fded3b8611e391b4f487d8785a8dbc5892d457bf
Tomikimi Ogoli <tomikimiogoli@yahoo.com>
2020-12-14 17:43:44 +0100

    Updated inmateService.js

--- src/services/inmateService.js
+++ src/services/inmateService.js
@@ -4,10 +4,16 @@ const apiEndPoint = "/inmate";
 
 export function saveInmateRecord(data) {
   return http.post(apiEndPoint, data, {
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
 }
 
 export function getInmateRecord() {
   return http.get(apiEndPoint);
+}
+
+export function getInmateRecordByID(id) {
+  return http.get(apiEndPoint + `/${id}`);
 }
